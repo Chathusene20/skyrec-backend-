c898cc78af25121991ad9aa82db6b821cdfd194b
Chathumini <chathuminisenethya246@gmail.com>
2026-07-23 22:26:19 +0530

    order authentication fix

--- routes/orderRouter.js
+++ routes/orderRouter.js
@@ -1,10 +1,11 @@
 import express from 'express';
 import { createOrder, getOrders, updateOrderStatus } from '../controllers/orderController.js';
+import verifyToken from '../middleware/middleware/verifyToken.js';
 
 const orderRouter = express.Router();
 
-orderRouter.post ("/", createOrder);
-orderRouter.get("/",getOrders)
-orderRouter.put("/status/orderID",updateOrderStatus)
+orderRouter.post ("/",verifyToken, createOrder);
+orderRouter.get("/",verifyToken,getOrders)
+orderRouter.put("/status/:orderID",verifyToken,updateOrderStatus)
 
 export default orderRouter;
