ee6aab2e9a79b5bda6c80beec5bb3314aa7a5336
Chathumini <chathuminisenethya246@gmail.com>
2026-08-01 14:09:24 +0530

    commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,53 +25,22 @@ const transporter = nodemailer.createTransport({
 
     requireTLS: true,
 
+    family: 4,
+
     
 
     auth: {
         user: process.env.EMAIL_USER,
         pass: process.env.APP_PASSWORD,
     },
-    tls: {
-    rejectUnauthorized: false
-    }
+    
+    
+    
 
    
 
 
 });
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 // Check mail server connection
 
